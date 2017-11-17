ce040deb94200bd1ec9f142e4fc2ff9fdb6318c5
Linh Sinh <sinhful@gmail.com>
2017-11-16 21:07:46 -0500

    Python Replace()
    Write a script translate.py that asks the user for some input and replace the characters. See exercise Real Python Part1 .

---
+++ translate.py
@@ -0,0 +1,11 @@
+
+user_input = input("Enter some text: ")
+new_user = user_input.replace("a", "4")\
+                     .replace("b", "8")\
+                     .replace("e", "3")\
+                     .replace("l", "1")\
+                     .replace("o","0")\
+                     .replace("s", "5")\
+                     .replace("t","7")
+print(new_user)
+print(user_input)
